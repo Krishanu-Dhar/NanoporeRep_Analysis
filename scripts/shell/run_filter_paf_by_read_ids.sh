bf522b371c36b791e99127f2b8e9c3e9b2e997d3
Krishanu-Dhar <krishanuxaverian2001@gmail.com>
2025-05-29 18:34:58 -0500

    Uploaded run_filter_paf_by_read_ids.sh
    This shell script is for running the filter_paf_by_read_ids.py script on a HPC cluster

---
+++ scripts/shell/run_filter_paf_by_read_ids.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This shell script runs a python script that extracts the metadata associated with a set of read IDs (TXT_FILE), sort them (based on alignment lengths)
+#SBATCH --job-name=run_filter_paf_by_read_ids
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
+#SBATCH --time=02:00:00
+#SBATCH --partition=serial
+
+# === Loading modules ===
+module purge
+module load slurm python pandas numpy matplotlib
+
+# === Paths and parameters ===
+TXT_FILE="/s/sansam-lab/rDNA_Rep/align_to_rdna/rdna_read_ids51.sorted.txt"
+PAF_FILE="/s/sansam-lab/rDNA_Rep/align_to_rdna/hits_barcode51.paf"
+OUTPUT="/s/sansam-lab/rDNA_Rep/align_to_rdna/Paf_hits/Paf_hits_barcode51.txt"
+LONG_READS="/s/sansam-lab/rDNA_Rep/align_to_rdna/Paf_hits/long_hits_barcode51_10K.txt"
+MIN_ALIGN=10000
+
+# === Run the script ===
+python3 /s/sansam-lab/rDNA_Rep/align_to_rdna/filter_paf_by_read_ids.py \
+  -r "$TXT_FILE" \
+  -p "$PAF_FILE" \
+  -o "$OUTPUT" \
+  -m "$MIN_ALIGN" \
+  -oL "$LONG_READS"
+
+# === Counting the number of reads that are above the minimum alignment length ===
+wc -l /s/sansam-lab/rDNA_Rep/align_to_rdna/Paf_hits/long_hits_barcode51_10K.txt
+# === Printing on run completion === #
+echo Successfully Ran
